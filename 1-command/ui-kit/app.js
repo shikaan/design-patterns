cc0d5fb36774aee5aa3467f0eee356a302eee9c0
shikaan <spagnolo.manu@gmail.com>
2018-11-10 15:53:43 +0100

    Command Pattern - Frontend Example

---
+++ 1-command/ui-kit/app.js
@@ -0,0 +1,107 @@
+// #===
+// Participants
+
+// Not too much to do without interfaces
+class Command {
+    /**
+     * @param {Receiver} receiver 
+     */
+    constructor(receiver) {
+        this.receiver = receiver
+    }
+    
+    execute() {}
+}
+
+// Concrete Command
+class OpenAlertCommand extends Command {
+    execute() {
+        this.receiver.alert('Gotcha!')
+    }
+}
+
+class AggregateCommand {
+    constructor(...commands) {
+        this.commands = commands
+    }
+
+    execute() {
+        this.commands.forEach(command => command.execute())
+    }
+}
+
+// Receiver
+class Window {
+    /**
+     * @param {string} message 
+     */
+    alert(message) {
+        window.alert(message)
+    }
+}
+
+// Receiver
+class Console {
+    /**
+     * @param {string} message 
+     * @param {Node} node 
+     */
+    alert(message) {
+        console.log('ALERT', message)
+    }
+}
+
+// Invoker
+class Button {
+    /**
+     * @param {string} label 
+     * @param {Command} command 
+     */
+    constructor(label, command) {
+        this.label = label
+        this.command = command
+        this.node = document.createElement('button')
+        
+        this.build()
+    }
+    
+    build() {
+        this.node.innerText = this.label
+        this.node.onclick = () => this.onClickHandler()
+    }
+    
+    onClickHandler() {
+        this.command.execute()
+    }
+}
+
+// Client
+class Application {
+    /**
+     * @param {Node} node 
+     */
+    constructor(node) {
+        this.node = node
+    }
+    
+    init() {
+        const windowReceiver = new Window()
+        const consoleReceiver = new Console()
+
+        const windowAlertCommand = new OpenAlertCommand(windowReceiver)
+        const consoleAlertCommand = new OpenAlertCommand(consoleReceiver)
+
+        const buttons = [
+            new Button('Window', windowAlertCommand),
+            new Button('Console', consoleAlertCommand),
+            new Button('Why choose?', new AggregateCommand(windowAlertCommand, consoleAlertCommand))
+        ]
+
+        buttons.forEach((button) => this.node.appendChild(button.node))
+    }
+}
+
+const appNode = document.getElementById('app')
+const application = new Application(appNode)
+
+application.init()
