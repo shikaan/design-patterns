96abbbeb41f3cebab5e1173d4418fd21d4d73a2f
manuel <spagnolo.manu@gmail.com>
2018-12-25 15:20:53 +0100

    Caching - Add --help for task runner

--- chistmas-caching/writing/tasks.py
+++ chistmas-caching/writing/tasks.py
@@ -3,23 +3,28 @@ from os.path import join
 
 virtualenv_folder = './virtualenv'
 
-@task
+@task(help="Launch the project")
 def start(c):
     with c.prefix("source " + join(virtualenv_folder, "bin/activate")):
         c.run("python3 src/main.py")
 
-@task
+@task(help="Initialize virtualenv for the current project")
 def init(c):
     c.run("mkdir -p " + virtualenv_folder)
     c.run("virtualenv " + virtualenv_folder)
 
-@task
-def install(c, module):
+@task(help={"module": "Module to be added as dependency"})
+def add(c, module):
     with c.prefix("source " + join(virtualenv_folder, "bin/activate")):
         c.run("pip3 install " + module)
         c.run("pip3 freeze > requirements.txt")
 
-@task
+@task(help="Install project's dependencies")
+def install(c):
+    with c.prefix("source " + join(virtualenv_folder, "bin/activate")):
+        c.run("pip3 install -r ./requirements.txt")
+
+@task(help="Launch test suite")
 def test(c):
     with c.prefix("source " + join(virtualenv_folder, "bin/activate")):
         c.run("py.test ./src/**_test.py")
